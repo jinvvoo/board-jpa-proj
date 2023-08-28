9589b56f862cf4929ebeee1f891095c5c481e49c
jinvvoo <jinvvookim@gmail.com>
2023-08-28 20:57:39 +0900

    02 list 기능 구현 완료

--- board/src/main/java/com/sixessense/board/dto/BoardDTO.java
+++ board/src/main/java/com/sixessense/board/dto/BoardDTO.java
@@ -23,12 +23,17 @@ public class BoardDTO {
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
 
-    public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
-        this.id = id;
-        this.boardWriter = boardWriter;
-        this.boardTitle = boardTitle;
-        this.boardHits = boardHits;
-        this.boardCreatedTime = boardCreatedTime;
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdatedTime());
+        return boardDTO;
     }
 
 }
